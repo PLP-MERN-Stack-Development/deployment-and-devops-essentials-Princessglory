d3ffdfdd166a916518cf5d6a072b1e29a86be979
Princess Glory <oprincessglory@gmail.com>
2025-11-19 14:11:12 +0100

    Configure GitHub Pages deployment for frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/deployment-and-devops-essentials-Princessglory/',
   server: {
     port: 5173,
     proxy: {
